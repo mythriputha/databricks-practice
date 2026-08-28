80bc7b089022fecb1e2deb4ac1fce65dce041934
puthamythri@gmail.com <puthamythri@gmail.com>
2026-08-28 04:09:47 +0000

    First Commit

---
+++ database_setup.sql
@@ -0,0 +1,4 @@
+CREATE CATALOG IF NOT EXISTS demo_catalog;
+CREATE SCHEMA IF NOT EXISTS demo_catalog.demo_schema;
+CREATE TABLE demo_catalog.demo_schema.customers
+SHOW CREATE TABLE demo_catalog.demo_schema.customers;
